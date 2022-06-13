b06e38d70cec2e6b634a3d4afcd25de501318dba
rakshith22-glitch <rakshith22@outlook.com>
2022-06-13 16:39:49 -0700

    server start

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 const port = process.env.PORT || 3000;
-const publicPath = path.join(__dirname, "../rakshithpfolio/public");
+const publicPath = path.join(__dirname, "../RAKSHITHPFOLIO/public");
 app.use(express.static(publicPath));
 
 if (process.env.NODE_ENV === "production") {
