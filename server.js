0cfcd64f1bd6437d58c9a76655d7da8fd1209ea8
rakshith22-glitch <rakshith22@outlook.com>
2024-05-06 08:38:14 -0700

    updated configs

--- server.js
+++ server.js
@@ -1,23 +1,31 @@
-const path = require("path");
-const express = require("express");
-const app = express();
+import path from "path";
+import express from "express";
+import { fileURLToPath } from 'url';
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = express();
 const port = process.env.PORT || 3000;
-const publicPath = path.join(__dirname, "../RAKSHITHPFOLIO/public");
+const publicPath = path.join(__dirname, "./portfolio/public");
+
+console.log(publicPath)
 app.use(express.static(publicPath));
 
-if (process.env.NODE_ENV === "production") {
+if (process.env.NODE_ENV !== "production") {
   // Serve any static files
-  app.use(express.static(path.join("build")));
+  app.use(express.static(path.join(__dirname, "public")));
   // Handle React routing, return all requests to React app
-  app.get("*", function (req, res) {
-    res.sendFile(path.join(__dirname, "build", "index.html"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "public", "index.html"));
   });
 }
+
 app.get("*", (req, res) => {
   res.sendFile(path.join(publicPath, "index.html"));
 });
+
 app.listen(port, (err) => {
-  if (err) return console.log(err);
+  if (err) return console.error(err);
   console.log("Server is up!", port);
 });
